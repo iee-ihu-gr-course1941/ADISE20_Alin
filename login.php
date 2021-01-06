8e678d6f5561c69c020908e8c40915ffe395f63b
nikosalin <joeyweler33@gmail.com>
2021-01-06 03:05:37 +0200

    lobby and and new login

--- login.php
+++ login.php
@@ -1,3 +1,13 @@
+<?php
+if(isset($_GET["error"])) {
+  if ($_GET["error"] == "emptyinput") {
+    echo "<p>συμπληρώστε ολα τα στοιχεία<p>";
+  }
+ else if ($_GET["error"] == "wronglogin"){
+    echo "<p>το ονομα/κωδικος δεν ειναι σωστα<p>";
+  }
+}
+ ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,7 +19,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 
-    
+
 </head>
 <body class="login-bg">
     <div class="container">
@@ -17,30 +27,29 @@
             <div class="header">
                 <h3>Σύνδεση</h3>
             </div>
-            <form action="#">
+          <form action="inc/login.inc.php" method="post">
                 <div class="username-login">
                     <label class="input-filled">
-                        <input required>
+                      <input type="text" name="username" required>
                         <span class="placeholder-span">Όνομα Χρήστη</span>
                     </label>
                     <p class="usererror"></p>
                 </div>
                 <div class="password">
                     <label class="input-filled">
-                        <input type="password" required>
+                        <input type="password" name ="password" required>
                         <span class="placeholder-span">Κωδικός Πρόσβασης</span>
                     </label>
                 </div>
                 <div class="btn">
                     <button type="submit" class="play-btn"><i class="fas fa-play"></i>επισκεπτης</button>
-                    <button type="submit">εισοδος</button>
+                    <button type="submit" name="submit">εισοδος</button>
                 </div>
                 <div class="create-acc">
                     <a href="register.php">Δημιουργία λογαριασμού</a>
-                </div>   
+                </div>
             </form>
         </div>
     </div>
-<script src="js/login.js"></script>
 </body>
 </html>
