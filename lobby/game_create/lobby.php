25a82e061198db0249c2437d3dcae134898f3723
tolismaloudis <73343451+tolismaloudis@users.noreply.github.com>
2021-01-06 03:34:20 +0200

    Add files via upload

---
+++ lobby/game_create/lobby.php
@@ -0,0 +1,22 @@
+<?php
+include_once "../freind.php";
+ ?>
+ <form action="../../inc/logout.inc.php" method="POST">
+    <button id="logout_btn" type="submit" name="logout"><i style="font-size: 23px;" class="fa fa-power-off"></i> Log Out</button>
+</form>
+  <button name="reload" id="btn_create" class="btn-success" type="reload" >reload</button>
+  <table id="container">
+    <tr>
+      <th>GAME NAME</th>
+      <th>OWNER</th>
+      <th>STATUS</th>
+    </tr>
+<?php
+  show_lobbys($conn);
+?>
+  </table>
+
+  <?php
+  include_once '../footer.php';
+
+     ?>
